2d5a87b16a9436eeb07a2f66e7c226513a7e971e
fdg312 <ffedorovzx@gmail.com>
2024-06-13 12:16:43 +0500

    jwt strategy(idk)

--- server/src/auth/jwt.strategy.ts
+++ server/src/auth/jwt.strategy.ts
@@ -1,29 +1,28 @@
-import { ExtractJwt, Strategy } from 'passport-jwt'
-import { PassportStrategy } from '@nestjs/passport'
-import { Injectable } from '@nestjs/common'
-import { getJwtConfig } from '../config/jwt.config'
-import { PrismaService } from '../prisma.service'
-import { User } from '@prisma/client'
-import {ConfigService} from "@nestjs/config";
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
-      private configService: ConfigService,
-      private prisma: PrismaService
+    private configService: ConfigService,
+    private prisma: PrismaService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: true,
-      secretOrKey: configService.get('JWT_SECRET')
-    })
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
   }
 
   async validate({ id }: Pick<User, 'id'>) {
     return this.prisma.user.findUnique({
       where: {
-        id: id
-      }
-    })
+        id: id,
+      },
+    });
   }
 }
